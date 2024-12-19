5e964909a7f5c84a6dd17827c9f4b62a19b336bd
kommichandana <chandana.kommi50@gmail.com>
2024-12-19 18:21:07 +0530

    Delete api for Addons

---
+++ Custom/Cleaning/Masters/V1/AddOns/dals/DeleteFuncs/EntryFile.js
@@ -0,0 +1,8 @@
+// import { StartFunc as StartFuncwriteFile } from '../../kLowDb/DeleteFromFile/DeleteRow.js';
+
+let DeleteFunc = ({ inId }) => {
+    return inId;
+    // return StartFuncwriteFile({inId});
+};
+
+export { DeleteFunc };
