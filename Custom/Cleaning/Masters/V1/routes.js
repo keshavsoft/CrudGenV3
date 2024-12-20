300242f7ad8d70fba696a0ee32b7f6aae852c202
Heyitsmeramya <ayitiramyasri@gmail.com>
2024-12-20 17:07:02 +0530

    delete for items

--- Custom/Cleaning/Masters/V1/routes.js
+++ Custom/Cleaning/Masters/V1/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as AddOns } from "./AddOns/routes.js";
+import { router as Items} from "./Items/routes.js";
 
 router.use('/AddOns', AddOns);
+router.use('/Items', Items);
 
 export { router };
