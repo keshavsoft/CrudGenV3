3eca3b9a7f47089bdf9be2a295a768665cd75145
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2025-01-03 18:34:25 +0530

    comments

--- Custom/Cleaning/Factory/V1/Inward/Scan/Voucher/routes/getFuncs/EntryFile.js
+++ Custom/Cleaning/Factory/V1/Inward/Scan/Voucher/routes/getFuncs/EntryFile.js
@@ -6,9 +6,9 @@ import {
     GetFunc, GetQrStatusFunc, GetRowDataFunc, GetAggregateFunc
 } from '../../controllers/getFuncs/EntryFile.js';
 
-router.get('/:inFactory', GetFunc);
-router.get('/QrStatus/:inFactory', GetQrStatusFunc);
-router.get('/RowData/:id', GetRowDataFunc);
-router.get('/Aggregate/:id', GetAggregateFunc);
+// router.get('/:inFactory', GetFunc);  //Not using this
+router.get('/QrStatus/:inFactory', GetQrStatusFunc); //using at DC status
+router.get('/RowData/:id', GetRowDataFunc); //Using at DC scan
+router.get('/Aggregate/:id', GetAggregateFunc); //Routes to Dal only
 
 export { router };
