b0ef4618144550ebb36b9b6ae47272cbd01759c4
keshavsoft <nknnkumar@yahoo.com>
2025-01-03 16:52:07 +0530

    Factory inward started

--- Custom/Cleaning/Factory/V1/Inward/Scan/Voucher/routes/getFuncs/EntryFile.js
+++ Custom/Cleaning/Factory/V1/Inward/Scan/Voucher/routes/getFuncs/EntryFile.js
@@ -4,13 +4,11 @@ var router = express.Router();
 
 import {
     GetFunc, GetQrStatusFunc, GetRowDataFunc, GetAggregateFunc
-}
-    from '../../controllers/getFuncs/EntryFile.js';
+} from '../../controllers/getFuncs/EntryFile.js';
 
 router.get('/:inFactory', GetFunc);
 router.get('/QrStatus/:inFactory', GetQrStatusFunc);
 router.get('/RowData/:id', GetRowDataFunc);
 router.get('/Aggregate/:id', GetAggregateFunc);
-
 
 export { router };
