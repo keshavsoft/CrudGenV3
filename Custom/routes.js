6926c0a234023f6b7419e09f0c8c1cfe1b993e0d
keshavsoft <nknnkumar@yahoo.com>
2025-01-05 17:16:19 +0530

    Google Contacts working good

--- Custom/routes.js
+++ Custom/routes.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 import { router as routerFromClientsFolder } from './Clients/routes.js';
 import { router as Cleaning } from './Cleaning/routes.js';
+// import { router as routerFromGContacts } from './GContacts/routes.js';
 
 router.use('/Clients', routerFromClientsFolder);
 router.use('/Cleaning', Cleaning);
+// router.use('/GContacts', routerFromGContacts);
 
 export { router };
