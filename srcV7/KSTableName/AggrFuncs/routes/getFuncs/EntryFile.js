0d66ad5065f586f564c795fe03e5785a6d9611e3
keshavsoft <nknnkumar@yahoo.com>
2025-01-06 17:10:53 +0530

    aggrfuncs fixed

--- srcV7/KSTableName/AggrFuncs/routes/getFuncs/EntryFile.js
+++ srcV7/KSTableName/AggrFuncs/routes/getFuncs/EntryFile.js
@@ -3,9 +3,7 @@ import express from 'express';
 var router = express.Router();
 
 import {
-    GetFunc, GetDataOnlyFunc, GetImagesFunc, GetBodyCheckFunc, GetFromModalFunc,
-    GetFromModalUuidFunc, GetWithJoinsFunc, GetDataSortByColumnFunc, GetRowDataFunc,
-    GetMaxRowFunc, GetLastRowFunc, GetFilterFunc
+    GetFunc, GetDataOnlyFunc
 } from '../../controllers/getFuncs/EntryFile.js';
 
 router.get('/', GetFunc);
