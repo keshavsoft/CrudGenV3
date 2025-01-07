16cba0c6a2b0722f37994163a14d1bd6bb0456fa
pushpadamalanka82 <y*********@gmail.com>
2025-01-07 20:19:31 +0530

    new endpoint

--- Custom/Cleaning/Branch/Factory/FromFactory/routes.js
+++ Custom/Cleaning/Branch/Factory/FromFactory/routes.js
@@ -7,7 +7,7 @@ import { router as routerFromEntryReturn } from './EntryReturn/routes.js';
 import { router as routerFromWashingReturn } from './WashingReturn/routes.js';
 import { router as Completion } from './Completion/routes.js';
 import { router as PressingReturn } from './PressingReturn/routes.js';
-
+import { router as Delivery } from './Delivery/routes.js';
 
 // import { router as routerFromReturn } from './Return/routes.js';
 
@@ -16,7 +16,7 @@ router.use('/EntryReturn', routerFromEntryReturn);
 router.use('/WashingReturn', routerFromWashingReturn);
 router.use('/Completion', Completion);
 router.use('/PressingReturn', PressingReturn);
-
+router.use('/Delivery', Delivery);
 
 // router.use('/Return', routerFromReturn);
 
