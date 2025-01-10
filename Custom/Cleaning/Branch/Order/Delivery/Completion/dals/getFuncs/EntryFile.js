33a5e75e45d47b3c56b6b1cf01010999b5618170
sharath0036 <ksharathbabu2@gmail.com>
2025-01-10 12:22:20 +0530

    delivery

--- Custom/Cleaning/Branch/Order/Delivery/Completion/dals/getFuncs/EntryFile.js
+++ Custom/Cleaning/Branch/Order/Delivery/Completion/dals/getFuncs/EntryFile.js
@@ -1,5 +1,6 @@
 import { StartFunc as GetOrderWithQr } from '../../kLowDb/ReadFileList/GetOrderWithQr.js';
 import { StartFunc as GetOrderSettlementWithQr } from '../../kLowDb/ReadFileList/GetOrderSettlementWithQr.js';
+import { StartFunc as GetCheckOrder } from '../../kLowDb/ReadFileList/GetCheckOrder.js';
 
 let GetFunc = ({ inOrderId, inBranch }) => {
     return GetOrderWithQr({ inOrderId, inBranch });
@@ -9,10 +10,14 @@ let GetOrderWithQrFunc = ({ inOrderId, inBranch  }) => {
     return GetOrderSettlementWithQr({ inOrderId, inBranch  });
 };
 
+let GetCheckFunc = ({ inOrderId, inBranch }) => {
+    return GetCheckOrder({ inOrderId, inBranch });
+};
+
+
 let GetRowDataFunc = ({ inId }) => {
     return GetOrderWithQr({ inId });
 };
-
 export {
-    GetFunc, GetOrderWithQrFunc, GetRowDataFunc
+    GetFunc, GetOrderWithQrFunc, GetRowDataFunc,GetCheckFunc
 };
--- Custom/Cleaning/Branch/Order/Delivery/Completion/repos/getFuncs/EntryFile.js
+++ Custom/Cleaning/Branch/Order/Delivery/Completion/repos/getFuncs/EntryFile.js
@@ -1,7 +1,8 @@
 import {
     GetFunc as GetFuncDal,
     GetOrderWithQrFunc as GetOrderWithQrFuncDal,
-    GetRowDataFunc as GetRowDataFuncDal
+    GetRowDataFunc as GetRowDataFuncDal,
+    GetCheckFunc as GetCheckFuncDal
 } from '../../dals/getFuncs/EntryFile.js';
 
 let GetFunc = ({ inOrderId, inBranch }) => {
@@ -12,10 +13,15 @@ let GetOrderWithQrFunc = ({ inOrderId, inBranch  }) => {
     return GetOrderWithQrFuncDal({ inOrderId, inBranch  });
 };
 
+
+let GetCheckFunc = ({ inOrderId, inBranch }) => {
+    return GetCheckFuncDal({ inOrderId, inBranch });
+};
+
 let GetRowDataFunc = ({ inId }) => {
     return GetRowDataFuncDal({ inId });
 };
 
 export {
-    GetFunc, GetOrderWithQrFunc, GetRowDataFunc
+    GetFunc, GetOrderWithQrFunc, GetRowDataFunc,GetCheckFunc
 };
