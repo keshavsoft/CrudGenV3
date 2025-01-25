6d6cbbe0c0b6cb01d13015f922614e456d9e41b3
pushpadamalanka82 <y*********@gmail.com>
2025-01-25 20:20:50 +0530

    status 200

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodes/BranchWise/3-QrCodes/controllers/GetFuncs/EntryFile.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodes/BranchWise/3-QrCodes/controllers/GetFuncs/EntryFile.js
@@ -4,14 +4,18 @@ import {
 } from '../../repos/GetFuncs/EntryFile.js';
 
 let GetFuncs = (req, res) => {
-    let LocalFromRepo = GetFuncsRepo();
+    const LocalFromRepo = GetFuncsRepo();
 
     if (LocalFromRepo.KTF === false) {
         res.status(500).send(LocalFromRepo.KReason);
         return;
     };
-
-    res.status(200).json(LocalFromRepo);
+    
+    res.set({
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+    }).status(200).json(LocalFromRepo);
 };
 
 let GetAsIsFuncs = (req, res) => {
