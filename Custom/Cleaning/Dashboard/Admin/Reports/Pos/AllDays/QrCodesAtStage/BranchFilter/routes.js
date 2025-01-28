90c7fda935163884c21bbdbe2b3c34b40274a97b
pushpadamalanka82 <y*********@gmail.com>
2025-01-28 16:43:59 +0500

    endpoints

--- Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodesAtStage/BranchFilter/routes.js
+++ Custom/Cleaning/Dashboard/Admin/Reports/Pos/AllDays/QrCodesAtStage/BranchFilter/routes.js
@@ -8,6 +8,10 @@ import { router as InWashing } from "./13-InWashing/routes.js";
 import { router as InPressing } from "./14-InPressing/routes.js";
 import { router as InCompletion } from "./15-Incompletion/routes.js";
 import { router as FactoryToBranch } from "./16-FactoryToBranch/routes.js";
+import { router as F_F_Entry_Return_Scan } from "./17-F_F_Entry_Return_Scan/routes.js"
+import { router as F_F_Pressing_Return_Scan } from "./18-F_F_Pressing_Return_Scan/routes.js"
+import { router as F_F_Completion_Scan } from "./19-F_F_Completion_Scan/routes.js"
+import { router as Delivery } from "./20-Delivery/routes.js"
 
 router.use("/InGenerate", InGenerate);
 router.use("/InBranch", InBranch);
@@ -15,5 +19,9 @@ router.use("/InWashing", InWashing);
 router.use("/InPressing", InPressing);
 router.use("/InCompletion", InCompletion);
 router.use("/FactoryToBranch", FactoryToBranch);
+router.use("/F_F_Entry_Return_Scan", F_F_Entry_Return_Scan);
+router.use("/F_F_Pressing_Return_Scan", F_F_Pressing_Return_Scan);
+router.use("/F_F_Completion_Scan", F_F_Completion_Scan);
+router.use("/Delivery", Delivery);
 
 export { router };
